075e027185d5ac0d29f0a01a79f7c73c0f3aedea
Vikas <vikask4590@gmail.com>
2023-11-05 14:22:47 +0530

    google analytics

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,10 @@ import Image from "next/image";
 import styles from "@/styles/Main.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <h1>Get Free Recipes</h1>
+      <h2>Coming Soon..</h2>
+    </main>
+  );
 }
