56d6788462b6409171cbc26fb1e0ad6329a37c67
Vikas <vikask4590@gmail.com>
2023-11-11 15:51:59 +0530

    proxy setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      // destination: "http://localhost:8000/api/:path*", // For development
+      destination: "https://freecodez-api.onrender.com/api/:path*", // For production
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
